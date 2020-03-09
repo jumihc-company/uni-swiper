bc2e88ae28e9c4ec0cb9f614c71628bdb8297398
ly <2944825244@qq.com>
2020-03-09 13:52:29 +0800

    chore:修改目录结构

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import jmhcSwiper from "src/components/jmhc-swiper.vue"
+import jmhcSwiper from "./src/components/swiper.vue"
 const swiper = {
     install(Vue, options) {
         Vue.component("jmhc-swiper", jmhcSwiper)
